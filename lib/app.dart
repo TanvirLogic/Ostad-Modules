52fd44b4d1bcced3f16b7f07363825bfe6ad05be
assignment <bmwthriad2023@gmail.com>
2025-08-11 12:42:01 +0600

    Module_12_Class_1

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:ostad_modules/Modules_11/class_3/water_tracker.dart';
+import 'package:ostad_modules/Module_12/class_1/expense_tracker.dart';
+
+import 'Module_11/class_3/water_tracker.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: WaterTracker());
+    return MaterialApp(home: MoneyManagment());
   }
 }
